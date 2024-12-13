0b606b076666a644b4aeade49893304dd7ed68dd
Timothy Tracy <55209004+Timothy-Tracy@users.noreply.github.com>
2024-12-13 01:29:23 -0800

    rm as in dm5

--- Comp584TaskApi/Program.cs
+++ Comp584TaskApi/Program.cs
@@ -102,11 +102,10 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthentication();
 app.UseAuthorization();

c65e3f1c62e56ec1703c196fc21c447c7baddfc7
Timothy Tracy <55209004+Timothy-Tracy@users.noreply.github.com>
2024-12-13 01:23:52 -0800

    rm as in dm4

--- Comp584TaskApi/Program.cs
+++ Comp584TaskApi/Program.cs
@@ -23,6 +23,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
